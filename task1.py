6276c08e27c3f303050812975773aaa7c62511d4
Avi <83268089+avirao007@users.noreply.github.com>
2025-06-22 13:05:34 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+n1=float(input('enter first number: '))
+n2=float(input('enter second number: '))
+a=n1+n2
+s=n1-n2
+m=n1*n2
+d=n1/n2
+print("addition ",a)
+print("subtraction ",s)
+print("multiplication ",m)
+print("division ",d)
---
+++ task2.py
@@ -0,0 +1,4 @@
+a1=input('enter first name: ')
+a2=input(' enter second name: ')
+fullname= (a1+" "+a2)
+print(f"Hello, {fullname}! welcome to the Python Program.")
